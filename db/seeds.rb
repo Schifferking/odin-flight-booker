1e408d12d7c50310defe3a60da277beb90dc422a
Schifferking <schifferking@hotmail.com>
2023-04-26 12:58:18 -0600

    Create Airport model
    It just contains a column called code, that contains an airport code.
    Also use db/seeds.rb to create 30 different airport records.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+codes = ['ATL', 'DFW', 'DEN', 'ORD', 'LAX', 'CLT', 'MCO', 'LAS', 'PHX', 'MIA',
+         'SEA', 'IAH', 'JFK', 'EWR', 'FLL', 'MSP', 'SFO', 'DTW', 'BOS', 'SLC',
+         'PHL', 'BWI', 'TPA', 'SAN', 'LGA', 'MDW', 'BNA', 'IAD', 'DCA', 'AUS']
+
+codes.each do |c|
+  Airport.create(code: c)
+end
