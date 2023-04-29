2bf02bae437f65cb6df2c4ce72a2a960812e2efd
Schifferking <schifferking@hotmail.com>
2023-04-29 11:22:31 -0600

    Feat: Add logic to search and display flights
    The Flight model contains scopes to query records based on each column
    of flights table: departure airport, arrival airport, number of
    passengers and flight date. It also has a method that returns all the
    dates to include them in the dropdown. Finally, it also contains a
    method to format a day (month/day/year).
    
    The flights controller now correctly requires and permits the parameters
    sent by the user when doing a search. In the index action, it
    initializes the flights instance variable making a query that returns
    zero records. It also has a new method that makes the search using the
    scopes. This method only executes when there is a "commit" param.
    
    In the index view, the form was changed to include a new dropdown that
    shows only the available dates in the flights table. It uses the dates
    method defined in the Flight model. This dropdown was created with the
    options_from_collection_for_select method. This method receives three
    arguments: a collection to iterate, a method to obtain the value from
    each element and another method to obtain the value that is displayed in
    each option from the dropdown.
    
    There is also a condition in the view that when the flights instance
    variable is empty, it displays a message saying that it found no flight
    when searching. Otherwise, it uses a loop and in a list element it shows
    the flight details (each attribute, with the date and hour formatted).

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,22 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.all
     @codes = Airport.all.map { |a| [a.code, a.id] }
     @tickets = [['1', 1], ['2', 2], ['3', 3], ['4', 4]]
+    @flights = Flight.dac(31)
+    obtain_flights if params.key? :commit
+  end
+
+  def params_required
+    params.require(:flight).permit(:departure_airport_id,
+                                   :arrival_airport_id,
+                                   :num_tickets,
+                                   :start)
+  end
+
+  def obtain_flights
+    @flights = Flight.dac(params_required[:departure_airport_id])
+                     .aac(params_required[:arrival_airport_id])
+                     .tickets(params_required[:num_tickets])
+                     .date(params_required[:start].to_datetime)
   end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,25 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
+
+  # Search by departure airport id
+  scope :dac, ->(id) { where(departure_airport_id: id) }
+
+  # Search by arrival airport id
+  scope :aac, ->(id) { where(arrival_airport_id: id) }
+
+  # Search by num of tickets
+  scope :tickets, ->(num) { where(num_tickets: num) }
+
+  # Search by start (date)
+  scope :date, ->(start) { where(start: (start.beginning_of_day..start.end_of_day)) }
+
+  # Get all dates
+  def self.dates
+    Flight.select(:start).order(:start)
+  end
+
+  def date_formatted
+    start.strftime("%m/%d/%Y")
+  end
 end
